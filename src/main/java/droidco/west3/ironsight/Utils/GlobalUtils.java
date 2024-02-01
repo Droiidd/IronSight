b2d09471f6d67e7985d0ec85b85a520678bcdfd2
jon pelaez <jonathan.pelaez@brightlysoftware.com>
2024-02-01 08:39:35 -0500

    Added basic item class, need class for UI icons, and MISC

--- src/main/java/droidco/west3/ironsight/Utils/GlobalUtils.java
+++ src/main/java/droidco/west3/ironsight/Utils/GlobalUtils.java
@@ -1,7 +1,11 @@
 package droidco.west3.ironsight.Utils;
 
+import droidco.west3.ironsight.Objects.Items.CustomItem;
+import droidco.west3.ironsight.Objects.Items.Rarity;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.units.qual.C;
 
 import java.util.Random;
 
@@ -29,5 +33,26 @@ public class GlobalUtils {
         else{
             return 0;
         }
+    }
+    public static void loadCustomItems()
+    {
+        //FOODS
+        CustomItem stew = new CustomItem("brown_stew","Brown Stew", Rarity.COMMON,
+                ChatColor.DARK_GRAY + "What's in this..?","", Material.MUSHROOM_STEW);
+        CustomItem charPot = new CustomItem("charred_potato","Charred Potato",Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Cooked on the coals.","",Material.BAKED_POTATO);
+        CustomItem cookFox = new CustomItem("cooked_fox","Cooked Fox", Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Bigger drumstick than chicken!","",Material.COOKED_CHICKEN);
+        CustomItem rabStew = new CustomItem("rabbit_stew","Rabbit Stew",Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Delicious with bread","",Material.RABBIT_STEW);
+        CustomItem iron = new CustomItem("iron_ore","Iron Ore",Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Can be refined or sold","",Material.IRON_ORE);
+        CustomItem copper = new CustomItem("copper_ore","Copper Ore", Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Can be refined or sold","",Material.COPPER_ORE);
+        CustomItem gold = new CustomItem("gold_ore","Gold Ore",Rarity.UNCOMMON,
+                ChatColor.DARK_GRAY+"Can be refined or sold", "",Material.GOLD_ORE);
+        CustomItem slug = new CustomItem("sea_slug","Slug",Rarity.COMMON,
+                ChatColor.DARK_GRAY+"Reaks of the sea","",Material.SPIDER_EYE);
+        
     }
 }
