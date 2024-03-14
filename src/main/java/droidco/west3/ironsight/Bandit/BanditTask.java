788087659326964213ad7507b5bd9f949ad83244
BuildTools <joncpelaezio@gmail.com>
2024-03-13 20:54:41 -0400

    rebasing

--- src/main/java/droidco/west3/ironsight/Bandit/BanditTask.java
+++ src/main/java/droidco/west3/ironsight/Bandit/BanditTask.java
@@ -61,150 +61,140 @@ public class BanditTask extends BukkitRunnable {
         BanditUtils.loadScoreBoard(p, b, combatLogTimer - combatLogCounter, wantedMin, wantedSec);
         // HANDLE PLAYER RESPAWN
         if (b.isRespawning()) {
-            // HANDLE PLAYER SEND TO PRISON
-            if (b.isJailedFlag()) {
-                b.setJailedFlag(false);
-                Location prison = Location.getLocation("Prison");
-                //Get the bukkit location of the respawn points from the Iron Sight Location (confusing)
-                org.bukkit.Location respawn = new org.bukkit.Location(p.getWorld(), prison.getSpawnX(), prison.getSpawnY(), prison.getSpawnZ());
-
-                p.sendTitle(ChatColor.GRAY + "You are now in" + ChatColor.DARK_RED + " Prison!", ChatColor.GRAY + "Mine to 0 bounty to leave.");
-                p.teleport(respawn);
-                p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 1);
-            }else{
-                p.setWalkSpeed(0);
-                p.setFlySpeed(0);
-                p.setSprinting(false);
-                if (p.getOpenInventory().getTitle().equalsIgnoreCase(ChatColor.DARK_GRAY + "Choose Town:") != true) {
-                    p.openInventory(LocationUI.openContractorTitleSelectUi(p));
-                }
+            p.setWalkSpeed(0);
+            p.setFlySpeed(0);
+            p.setSprinting(false);
+            if (p.getOpenInventory().getTitle().equalsIgnoreCase(ChatColor.DARK_GRAY + "Choose Town:") != true) {
+                p.openInventory(LocationUI.openContractorTitleSelectUi(p));
             }
         }
+        // HANDLE PLAYER SEND TO PRISON
+        if (b.isJailedFlag()) {
+            b.setJailedFlag(false);
+            Location prison = Location.getLocation("Prison");
+            //Get the bukkit location of the respawn points from the Iron Sight Location (confusing)
+            org.bukkit.Location respawn = new org.bukkit.Location(p.getWorld(), prison.getSpawnX(), prison.getSpawnY(), prison.getSpawnZ());
 
+            p.sendTitle(ChatColor.GRAY + "You are now in" + ChatColor.DARK_RED + " Prison!", ChatColor.GRAY + "Mine to 0 bounty to leave.");
+            p.teleport(respawn);
+            p.playSound(p.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, 1);
+        }
         //HANDLE LOCATION SPECIFIc
         Location currentLoc = b.getCurrentLocation();
-//
-//        //TOWNS
-//        if (currentLoc.getType().equals(LocationType.TOWN)) {
-//            //p.sendMessage("You cannot damage players in town!");
-//            p.setLastDamage(0.0);
-//            //NO WANTED PLAYERS IN TOWN!!!
-//            //Give them ten seconds to leave before killing them
-//            if (b.isWanted()) {
-//                if (tick % 3 == 0) {
-//                    //DISPLAY HOW LONG THEY HAVE TO LEAVE BEFORE KILLING THEM
-//                    if (wantedTownCounter == 0) {
-//                        //TIMER JUST STARTED!
-//                        p.sendMessage("Wanted players not allowed in towns, leave or die.");
-//                    }
-//                    p.sendMessage((wantedTownTimer - wantedTownCounter) + " seconds to leave.");
-//                    if (wantedTownCounter == wantedTownTimer) {
-//                        p.damage(100);
-//                    }
-//                    wantedTownCounter++;
-//                }
-//            }
-//
-//        }
-//        //ILLEGAL
-//        if (currentLoc.getType().equals(LocationType.ILLEGAL)) {
-//            //Increase players bounty in illegal area
-//            if (tick % 3 == 0) {
-//                b.updateBounty(3);
-//            }
-//        }
-//        if (b.isJailed()) {
-//            if (b.getCurrentLocation() == null || !currentLoc.getType().equals(LocationType.Prison)) {
-//                escaping = true;
-//                //Player is escaping! PUT LOGIC HERE
-//                p.sendMessage("escapee");
-//            }
-//        }
 
-//        if (b.isJailed()) {
-//            p.sendMessage("JAILED");
-//            if (currentLoc.getType().equals(LocationType.Prison)) {
-//                if (tick % 3 == 0) {
-//                    //Player is jailed and in prison
-////                    if (escaping == true) {
-////                        escaping = false;
-////                        this.prisonEscapeTimer.replace(p.getUniqueId().toString(), null);
-////                    }
-//                    long currentTime = System.currentTimeMillis();
-//                    long elapsedTime = currentTime - b.getJailStartTime();
-//                    int elap = (int) elapsedTime / 1000;
-//
-//                    b.updateBounty(-1);
-//                    //p.sendMessage(elap);
-//                    if (elap >= b.getBounty()) {
-//                        //Player has waited enough time
-//                        p.sendMessage("ELAP");
-//                        releasePrisoner(b);
-//                    }
-//                }
-//                if (b.getBounty() <= 0) {
-//                    p.sendMessage("NO BOUNTY");
-//                    releasePrisoner(b);
-//                }
-//
-//            }else{
-//                p.sendMessage("ESCAPE");
-//            }
-//        }
+        //TOWNS
+        if (currentLoc.getType().equals(LocationType.TOWN)) {
+            //p.sendMessage("You cannot damage players in town!");
+            p.setLastDamage(0.0);
+            //NO WANTED PLAYERS IN TOWN!!!
+            //Give them ten seconds to leave before killing them
+            if (b.isWanted()) {
+                if (tick % 3 == 0) {
+                    //DISPLAY HOW LONG THEY HAVE TO LEAVE BEFORE KILLING THEM
+                    if (wantedTownCounter == 0) {
+                        //TIMER JUST STARTED!
+                        p.sendMessage("Wanted players not allowed in towns, leave or die.");
+                    }
+                    p.sendMessage((wantedTownTimer - wantedTownCounter) + " seconds to leave.");
+                    if (wantedTownCounter == wantedTownTimer) {
+                        p.damage(100);
+                    }
+                    wantedTownCounter++;
+                }
+            }
+
+        }
+        //ILLEGAL
+        if (currentLoc.getType().equals(LocationType.ILLEGAL)) {
+            //Increase players bounty in illegal area
+            if (tick % 3 == 0) {
+                b.updateBounty(3);
+            }
+        }
+        if (b.isJailed()) {
+            if (b.getCurrentLocation() == null || !currentLoc.getType().equals(LocationType.Prison)) {
+                escaping = true;
+                //Player is escaping! PUT LOGIC HERE
+                p.sendMessage("escapee");
+            }
+        }
+        if (b.isJailed()) {
+            if (currentLoc.getType().equals(LocationType.Prison)) {
+                if (tick % 3 == 0) {
+                    //Player is jailed and in prison
+                    if (escaping == true) {
+                        escaping = false;
+                        this.prisonEscapeTimer.replace(p.getUniqueId().toString(), null);
+                    }
+                    //p.setLevel();
+                    long currentTime = System.currentTimeMillis();
+                    long elapsedTime = currentTime - b.getJailStartTime();
+                    int elap = (int) elapsedTime / 1000;
+
+                    //b.updateBounty(-1);
+                    //p.sendMessage(elap);
+                    if (elap >= b.getBounty()) {
+                        //Player has waited enough time
+                        p.sendMessage("ELAP");
+                        releasePrisoner(b);
+                    }
+                }
+                if (b.getBounty() <= 0) {
+                    p.sendMessage("NO BOUNTY");
+                    releasePrisoner(b);
+                }
+
+            }
+        }
         //TIMED EVENTS
 
         //Roughly 1 second
         if (tick % 3 == 0) {
-            if(b.isJailed()){
-                p.sendMessage("jailed");
-            }else{
-                p.sendMessage("not jailed?");
-            }
             // HANDLE WANTED TIMER
-//            if (b.isWanted()) {
-//                wantedSec--;
-//                if (wantedSec == -1) {
-//                    wantedSec = 59;
-//                    wantedMin--;
-//                }
-//                if (wantedMin == -1) {
-//                    //Timer is complete!
-//                    wantedMin = 2;
-//                    p.sendMessage(ChatColor.GRAY + "You are no longer " + ChatColor.DARK_RED + "wanted.");
-//                    b.setWanted(false);
-//                }
-//            }
-//            // HANDLE COMBAT BLOCK TIMER
-//            if (b.isCombatBlocked()) {
-//                if (b.isCombatBlockFlag()) {
-//                    combatLogCounter = 0;
-//                    b.setCombatBlockFlag(false);
-//                }
-//                if (combatLogCounter == combatLogTimer) {
-//                    b.setCombatBlocked(false);
-//                    p.sendMessage(ChatColor.GRAY + "You are" + ChatColor.GREEN + " safe " + ChatColor.GRAY + "to log off");
-//                }
-//                combatLogCounter++;
-//            }
-//            if (b.isWanted()) {
-//
-//            }
-//            //HANDLE CONTRACT TIMER
-//
-//            p.setLevel(contractTimer - contractCounter);
-//            if (contractTimer == contractCounter) {
-//                ContractUtils.initializeContracts(b);
-//                p.sendMessage(ChatColor.GOLD + "Contracts" + ChatColor.GREEN + " reset!");
-//                contractCounter = 0;
-//            }
-//            contractCounter++;
-//            // HANDLE PLAYER BLEED EFFECT
-//            if (b.isBleeding()) {
-//                p.setHealth(p.getHealth() - 1.5);
-//                for (int i = 0; i < 13; i++) {
-//                    p.spawnParticle(Particle.BLOCK_DUST, p.getLocation().add(0.5, 0.5, 0.5), 1, 1, 1, 1, 1, new ItemStack(Material.RED_WOOL));
-//                }
-//            }
+            if (b.isWanted()) {
+                wantedSec--;
+                if (wantedSec == -1) {
+                    wantedSec = 59;
+                    wantedMin--;
+                }
+                if (wantedMin == -1) {
+                    //Timer is complete!
+                    wantedMin = 2;
+                    p.sendMessage(ChatColor.GRAY + "You are no longer " + ChatColor.DARK_RED + "wanted.");
+                    b.setWanted(false);
+                }
+            }
+            // HANDLE COMBAT BLOCK TIMER
+            if (b.isCombatBlocked()) {
+                if (b.isCombatBlockFlag()) {
+                    combatLogCounter = 0;
+                    b.setCombatBlockFlag(false);
+                }
+                if (combatLogCounter == combatLogTimer) {
+                    b.setCombatBlocked(false);
+                    p.sendMessage(ChatColor.GRAY + "You are" + ChatColor.GREEN + " safe " + ChatColor.GRAY + "to log off");
+                }
+                combatLogCounter++;
+            }
+            if (b.isWanted()) {
+
+            }
+            //HANDLE CONTRACT TIMER
+
+            p.setLevel(contractTimer - contractCounter);
+            if (contractTimer == contractCounter) {
+                ContractUtils.initializeContracts(b);
+                p.sendMessage(ChatColor.GOLD + "Contracts" + ChatColor.GREEN + " reset!");
+                contractCounter = 0;
+            }
+            contractCounter++;
+            // HANDLE PLAYER BLEED EFFECT
+            if (b.isBleeding()) {
+                p.setHealth(p.getHealth() - 1.5);
+                for (int i = 0; i < 13; i++) {
+                    p.spawnParticle(Particle.BLOCK_DUST, p.getLocation().add(0.5, 0.5, 0.5), 1, 1, 1, 1, 1, new ItemStack(Material.RED_WOOL));
+                }
+            }
         }
         //END OF LOOP
         tick++;
