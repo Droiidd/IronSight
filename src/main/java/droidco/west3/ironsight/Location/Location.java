40a19d0054c0e92bcb3a48e2194df1565d372f15
BuildTools <unconfigured@null.spigotmc.org>
2024-03-21 12:13:55 -0400

    finished icons and player tracking logic

--- src/main/java/droidco/west3/ironsight/Location/Location.java
+++ src/main/java/droidco/west3/ironsight/Location/Location.java
@@ -12,29 +12,31 @@ import java.util.Map;
 
 public class Location {
     private String locName;
-    private double x1,x2,z1,z2;
-    private double spawnX,spawnY,spawnZ;
+    private double x1, x2, z1, z2;
+    private double spawnX, spawnY, spawnZ;
     private String welcomeMessage;
     private final BossBar locTitle;
-    private static final BossBar wildernessTitle =Bukkit.createBossBar("Wilderness", BarColor.GREEN, BarStyle.SOLID);
+    private static final BossBar wildernessTitle = Bukkit.createBossBar("Wilderness", BarColor.GREEN, BarStyle.SOLID);
     private LocationType type;
-    private static HashMap<String,Location> locations = new HashMap<>();
+    private static HashMap<String, Location> locations = new HashMap<>();
 
-    public Location(String locName, String welcomeMessage, LocationType type, double x1, double x2, double z1, double z2){
+    public Location(String locName, String welcomeMessage, LocationType type, double x1, double x2, double z1, double z2) {
         this.locName = locName;
-        this.x1= x1;
+        this.x1 = x1;
         this.x2 = x2;
         this.z1 = z1;
         this.z2 = z2;
         this.welcomeMessage = welcomeMessage;
         this.type = type;
 
-        this.locTitle = getTitleBossBar(locName,getTitleColor(type));
+        this.locTitle = getTitleBossBar(locName, getTitleColor(type));
 
-        locations.put(locName,this);
-    }public Location(String locName,String welcomeMessage, LocationType type, double x1, double x2, double z1, double z2,double spawnX, double spawnY, double spawnZ){
+        locations.put(locName, this);
+    }
+
+    public Location(String locName, String welcomeMessage, LocationType type, double x1, double x2, double z1, double z2, double spawnX, double spawnY, double spawnZ) {
         this.locName = locName;
-        this.x1= x1;
+        this.x1 = x1;
         this.x2 = x2;
         this.z1 = z1;
         this.z2 = z2;
@@ -44,32 +46,32 @@ public class Location {
         this.welcomeMessage = welcomeMessage;
         this.type = type;
 
-        this.locTitle = getTitleBossBar(locName,getTitleColor(type));
+        this.locTitle = getTitleBossBar(locName, getTitleColor(type));
 
-        locations.put(locName,this);
+        locations.put(locName, this);
     }
 
-    public static HashMap<String,Location> getLocations()
-    {
+    public static HashMap<String, Location> getLocations() {
         return locations;
     }
-    public static boolean isPlayerInWilderness(Player p)
-    {
-        for (Map.Entry<String,Location> mapE : locations.entrySet()) {
-            if(mapE.getValue().isPlayerInside(p)){
+
+    public static boolean isPlayerInWilderness(Player p) {
+        for (Map.Entry<String, Location> mapE : locations.entrySet()) {
+            if (mapE.getValue().isPlayerInside(p)) {
                 return false;
             }
         }
         return true;
     }
-    public static Location getLocation(String locationName){
-        if(locations.containsKey(locationName)){
+
+    public static Location getLocation(String locationName) {
+        if (locations.containsKey(locationName)) {
             return locations.get(locationName);
         }
         return null;
     }
-    public boolean isPlayerInside(Player p)
-    {
+
+    public boolean isPlayerInside(Player p) {
         double minX;
         double maxX;
         double minZ;
@@ -92,76 +94,79 @@ public class Location {
             maxZ = z1;
         }
         //PLAYER IS WITHIN THE ZONE
-        if((playerX > minX && playerX < maxX) && (playerZ > minZ && playerZ < maxZ)){
+        if ((playerX > minX && playerX < maxX) && (playerZ > minZ && playerZ < maxZ)) {
             return true;
         }
         return false;
     }
 
 
-    public static void displayLocation(Player p)
-    {
+    public static void displayLocation(Player p) {
         Bandit b = Bandit.getPlayer(p);
         locations.forEach((s, location) -> {
-            if(location.isPlayerInside(p)){
+            if (location.isPlayerInside(p)) {
                 location.addTitle(p);
                 b.setCurrentLocation(location);
-            }else{
+            } else {
                 location.removeTitle(p);
             }
 
         });
         //p.sendMessage(""+locations.get("test").isPlayerInside(p));
-        if(Location.isPlayerInWilderness(p)){
+        if (Location.isPlayerInWilderness(p)) {
             //Display wilderness
             Location.displayWilderness(p);
             b.setCurrentLocation(getLocation("Wilderness"));
-        }else{
+        } else {
             //Else check the towns.
             Location.removeWilderness(p);
         }
     }
-    public BossBar getTitleBossBar(String title, BarColor color)
-    {
+
+    public BossBar getTitleBossBar(String title, BarColor color) {
         return Bukkit.createBossBar(title,
                 color,
                 BarStyle.SOLID);
     }
-    public org.bukkit.Location getSpawnLocation(Player p){
-        return new org.bukkit.Location(p.getWorld(),spawnX,spawnY,spawnZ);
+
+    public org.bukkit.Location getSpawnLocation(Player p) {
+        return new org.bukkit.Location(p.getWorld(), spawnX, spawnY, spawnZ);
     }
-    public void addTitle(Player p){
+
+    public void addTitle(Player p) {
         this.locTitle.setProgress(1);
         this.locTitle.addPlayer(p);
         this.locTitle.setVisible(true);
     }
-    public void removeTitle(Player p){
+
+    public void removeTitle(Player p) {
         this.locTitle.removePlayer(p);
         this.locTitle.setVisible(false);
     }
-    public static void displayWilderness(Player p){
+
+    public static void displayWilderness(Player p) {
         wildernessTitle.setProgress(1);
         wildernessTitle.addPlayer(p);
         wildernessTitle.setVisible(true);
     }
-    public static void removeWilderness(Player p){
+
+    public static void removeWilderness(Player p) {
         wildernessTitle.removePlayer(p);
         wildernessTitle.setVisible(false);
     }
-    public BarColor getTitleColor(LocationType type){
-        if(type.compareTo(LocationType.EVENT) == 0){
+
+    public BarColor getTitleColor(LocationType type) {
+        if (type.compareTo(LocationType.EVENT) == 0) {
             return BarColor.YELLOW;
-        }else if(type.compareTo(LocationType.TOWN) == 0){
+        } else if (type.compareTo(LocationType.TOWN) == 0) {
             return BarColor.PINK;
-        }
-        else if(type.compareTo(LocationType.ILLEGAL) ==0){
+        } else if (type.compareTo(LocationType.ILLEGAL) == 0) {
             return BarColor.RED;
-        }else if(type.compareTo(LocationType.NATURAL) == 0){
+        } else if (type.compareTo(LocationType.NATURAL) == 0) {
             return BarColor.PURPLE;
-        }else if(type.compareTo(LocationType.River) == 0){
+        } else if (type.compareTo(LocationType.River) == 0) {
             return BarColor.BLUE;
-        }
-        else{
+        } else {
             return BarColor.GREEN;
         }
     }
@@ -251,4 +256,17 @@ public class Location {
     public void setType(LocationType type) {
         this.type = type;
     }
+
+    public org.bukkit.Location getCenterLocation(Player p) {
+        double x3 = (this.x1 + this.x2) / 2;
+        double z3 = (this.z1 + this.z2) / 2;
+        return new org.bukkit.Location(p.getWorld(), x3, 100.0, z3);
+    }
+
+
+
+
+
 }
+
+
